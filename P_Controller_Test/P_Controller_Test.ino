9d158c211b968d65138d95bde716e8d8c829392b
Andrew Jevitt <51527902+andrewjevitt@users.noreply.github.com>
2022-10-22 00:06:04 -0400

    ENH: added quadratic approximation of position
    Implemented Quadratic Approx Method from PATH PLANNING OF LINE FOLLOWER ROBOT by Mustafa Engin and Dilşad Engin. Center is now 5000 since position range with this method is 0-10,000. Implements P controller, but has full PID code.

--- P_Controller_Test/P_Controller_Test.ino
+++ P_Controller_Test/P_Controller_Test.ino
@@ -3,8 +3,8 @@
 #include "Romi_Motor_Power.h"
 
 // Debug Control
-#define DEBUG 0
-#define PLX 1
+#define DEBUG 1
+#define PLX 0
 
 #if DEBUG == 1
 #define debugln(x) Serial.println(x)
@@ -43,11 +43,20 @@ uint16_t sensorMinVal[LS_NUM_SENSORS];
 
 
 int numRuns;
+int error = 0;
 
 // P Controller
-double Ke = 1.5*(0.5 / 3500); // Range of output/max error
+double Ke = 0.5/7000; // Range of output/max error
 double DR = 0.5;
-volatile int center = 3500;
+volatile int center = 5000;
+
+// I controller
+double Ki = 0.0000;
+double Kd = 0.00000;//73925;
+volatile unsigned long TimeStart = millis();
+volatile unsigned long TimeEnd= millis();
+double i;
+
 
 void setup()
 {
@@ -100,19 +109,70 @@ void floorCalibration()
   setMotorDirection(BOTH_MOTORS, MOTOR_DIR_FORWARD);
 }
 
+ uint32_t getMyLinePosition(uint16_t* calVal, uint8_t mode)
+ {
+  // calVal is 0 to 1000 based on each sensor
+   uint32_t _lastPosition;
+   
+   //Quadratic Approx Method
+   //Determine i1, i2, i3 as the 3 highest consecutive numbers' indices
+    
+  int i, max_i = 0;
+  float m = calVal[0];
+  for(i = 1; i < LS_NUM_SENSORS; ++i){
+      if(calVal[i] > m){
+          m = calVal[i];
+          max_i = i;
+      }
+  }
+  // max_i is the index if the max
+  uint8_t i1;
+   if (max_i == LS_NUM_SENSORS-1){
+    i1 = max_i - 2;
+   } else if (max_i == 0){
+    i1 = 0;
+   } else{
+    i1 = max_i - 1;
+   }
+     
+   uint8_t i2 = i1 + 1;
+   uint8_t i3 = i1 + 2;
+   unsigned long i11 = i1 * i1;
+   unsigned long i22 = i2 * i2;
+   unsigned long i33 = i3 * i3;
+   uint16_t y1 = calVal[i1];
+   uint16_t y2 = calVal[i2];
+   uint16_t y3 = calVal[i3];
+   double a = (y1 + y2 - 2*y3)/2;
+   double b = (y1*(i22-i33)+y2*(i33-i11)+y3*(i11-i22))/2;
+   //double c; //not needed as x = -b/2a
+   double x1 = -b / (2 * a);
+   double position = ((y2-y1-a*(2*x1 + 1))*100/103);
+   
+   if (position > 11000){
+    _lastPosition = _lastPosition;
+   } else{
+    _lastPosition = (uint32_t)position;
+   }
+   
+   _lastPosition = constrain(_lastPosition,0,10000);
+   return _lastPosition;
+ }
+
 uint32_t getPosition()
 {
   readLineSensor(sensorVal);
   readCalLineSensor(sensorVal, sensorCalVal, sensorMinVal, sensorMaxVal, lineColor);
   // could use average
   
-  return getLinePosition(sensorCalVal, lineColor);
+  return getMyLinePosition(sensorCalVal, lineColor);
   
 }
 
 
 void loop()
 {
+  
   uint32_t lastPos;
   /* Run this setup only once */
   if (isCalibrationComplete == false)
@@ -124,13 +184,25 @@ void loop()
 
 
   uint32_t linePos = getPosition();
-  int error = linePos - center;
+  //Adding the integration and dif.
+  TimeEnd = millis();
   
-
+  unsigned long dTime = TimeEnd - TimeStart;
   
-  double adjustment = error * Ke;
+  i += dTime*(error);
+  double d = (error)/dTime;
+  TimeStart = millis();
+  
+  error = linePos - center;
+  i -= dTime*error;
+  d -= (error)/dTime;
+  
+   
+  
+  double adjustment = (error * Ke) + (i*Ki)+ (d*Kd);
   double DRnow = DR + adjustment;
   DRnow= constrain(DRnow, 0, 1);
+  
   setRawMotorSpeed(LEFT_MOTOR, ((DRnow)*Speed));
   setRawMotorSpeed(RIGHT_MOTOR, ((1 - DRnow) * Speed));
 
@@ -148,7 +220,7 @@ void loop()
   PLXout(" ,");
   PLXout(error);
   PLXout(" ,");
-//  PLXout(estPos);
+  PLXout(dTime);
   PLXoutln(" ,");
   //delay(500);
   
